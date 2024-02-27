a2963479443ca27f034fe84dc423f91b3dbf11ec
Zsieteo <160556988+Zsieteo@users.noreply.github.com>
2024-02-27 12:01:29 +0800

    Add files via upload

---
+++ 122214230/src/hello.java
@@ -0,0 +1,13 @@
+
+public class hello {
+		static String name = "8";
+		static int age = 4;
+		static String gender;
+	public static void main(String[] args) {
+		
+		
+		// TODO Auto-generated method stub
+		System.out.println("My name is "+name+" age is "+age+" gender is "+gender);
+	}
+
+}
